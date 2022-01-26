54fc8d53b28f81ea6285ca1d879281d35672995a
sleabykhalil <khalilsleaby@hotmail.com>
2022-01-26 16:12:00 +0100

    init patient ui

---
+++ patientui/src/test/java/com/mediscreen/patientui/PatientUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.patientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
