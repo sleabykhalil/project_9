5d7a3a82b2fb2ee214aba0dd3fdbbf1f59675adf
sleabykhalil <khalilsleaby@hotmail.com>
2022-01-26 09:53:54 +0100

    Add doc to test

--- patientApi/src/test/java/com/mediscreen/patientApi/controller/PatientControllerTest.java
+++ patientApi/src/test/java/com/mediscreen/patientApi/controller/PatientControllerTest.java
@@ -5,30 +5,36 @@ import com.mediscreen.patientApi.utils.JsonUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.sql.Date;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
+//@ExtendWith(SpringExtension.class)
+@ExtendWith({ RestDocumentationExtension.class, SpringExtension.class })
+//@AutoConfigureMockMvc
 class PatientControllerTest {
 
-    @Autowired
+   // @Autowired
     MockMvc mockMvc;
 
     PatientDto patientDto;
 
     @BeforeEach
-    void setUp() {
+    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
         patientDto = PatientDto.builder()
                 .firstName("firstName")
                 .lastName("lastName")
@@ -36,6 +42,11 @@ class PatientControllerTest {
                 .address("address")
                 .gender("male")
                 .phoneNumber("01 23 45 67 89").build();
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation))
+                .alwaysDo(document("{method-name}", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
+                .build();
 
     }
 
