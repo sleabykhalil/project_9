08886d9fa62a5acc0182bcd616cc009a8fe1969a
sleabykhalil <khalilsleaby@hotmail.com>
2022-03-28 10:35:22 +0200

    Add find medical history by patientId

--- patientui/src/main/java/com/mediscreen/patientui/proxies/PatientMedicalHistoryProxy.java
+++ patientui/src/main/java/com/mediscreen/patientui/proxies/PatientMedicalHistoryProxy.java
@@ -1,9 +1,12 @@
 package com.mediscreen.patientui.proxies;
 
+import com.mediscreen.patientui.dto.MedicalHistoryDto;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Set;
 
 @FeignClient(name = "medicalhistory", url = "http://microservice.patientmedicalhistory:8082")
@@ -11,4 +14,11 @@ public interface PatientMedicalHistoryProxy {
 
     @GetMapping("/patient_medical_history/aggregateMedicalHistory")
     Set<String> aggregateMedicalHistory(@RequestParam String id);
-}
+
+    @GetMapping
+    MedicalHistoryDto findMedicalHistory(@RequestParam @Parameter(description = "medical record id", required = true) String id) ;
+
+    @GetMapping("/patient_medical_history/findbypatientid")
+    List<MedicalHistoryDto> findByPatientId(@RequestParam String patientId) ;
+
+    }
